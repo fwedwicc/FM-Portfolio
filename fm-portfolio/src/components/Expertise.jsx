1c40fc3a26953a81829929972568923caa1e47a6
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-06-14 16:23:10 +0800

    Modified github stats' layout and styles

--- fm-portfolio/src/components/Expertise.jsx
+++ fm-portfolio/src/components/Expertise.jsx
@@ -2,15 +2,22 @@ import React from 'react'
 
 const Expertise = () => {
   return (
-    <div className='flex justify-center items-center h-full'>
-      <div align="center">
-        <img src="https://github-readme-stats.vercel.app/api/top-langs?username=fwedwicc&locale=en&hide_title=true&layout=compact&card_width=350&langs_count=8&order=2&bg_color=1C182D&text_color=C1B1FF&border_color=40317A" className='h-[12rem] w-auto' alt="languages graph" />
+    <div className='lg:p-6 p-4 flex flex-col gap-[2rem]'>
+      <div className='flex justify-center items-center'>
 
-        <img src="https://streak-stats.demolab.com?user=fwedwicc&locale=en&theme=transparent&bg_color=1C182D&text_color=C1B1FF&hide_border=true&order=3" className='h-[13rem] w-auto bg-[#1C182D] border-[#40317A] border' alt="streak graph" />
+        < div className='flex flex-col gap-6' >
+          <h1 className='uppercase text-white font-black text-[2.5rem] lg:text-[5rem] whitespace-nowrap leading-none text-center mb-[2rem]' id='scroll-animation-'>Github Stats</h1>
+          <div className='flex justify-center items-start gap-6 flex-wrap'>
+            {/* Language Graph */}
+            <img src="https://github-readme-stats.vercel.app/api/top-langs?username=fwedwicc&locale=en&hide_title=true&layout=compact&card_width=350&langs_count=8&order=2&bg_color=1C182D&text_color=C1B1FF&border_color=40317A" className='h-[12rem] w-auto' alt="languages graph" />
+            {/* Streak Graph */}
+            <img src="https://streak-stats.demolab.com?user=fwedwicc&locale=en&theme=transparent&bg_color=1C182D&text_color=C1B1FF&hide_border=true&order=3" className='h-[12rem] w-auto bg-[#1C182D] border-[#40317A] border rounded-lg' alt="streak graph" />
+          </div>
+          {/* Contribution Graph */}
+          <img src="https://github-readme-activity-graph.vercel.app/graph?username=fwedwicc&radius=7&theme=material-palenight&bg_color=1C182D&text_color=ffffff&area=true&order=5&hide_title=true&hide_border=true" className='lg:h-[20rem] h-[13rem] w-auto rounded-lg' alt="activity-graph graph" />
+        </div >
 
-        <img src="https://github-readme-activity-graph.vercel.app/graph?username=fwedwicc&radius=7&theme=material-palenight&bg_color=1C182D&text_color=ffffff&area=true&order=5&hide_title=true&hide_border=true" className='h-[20rem] w-auto border-[#40317A] border rounded-lg' alt="activity-graph graph" />
 
-        
 
       </div>
     </div>
@@ -18,3 +25,5 @@ const Expertise = () => {
 }
 
 export default Expertise
+
+
