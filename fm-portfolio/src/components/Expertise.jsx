e2c7377562f2eb0cfa1a98c6dd061fdc7164ef24
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-06-15 12:24:57 +0800

    Fixed styling in streak card in github stats

--- fm-portfolio/src/components/Expertise.jsx
+++ fm-portfolio/src/components/Expertise.jsx
@@ -21,7 +21,7 @@ const GithubStats = () => {
           {/* Language Graph */}
           <img src="https://github-readme-stats.vercel.app/api/top-langs?username=fwedwicc&locale=en&hide_title=true&layout=compact&card_width=350&langs_count=8&order=2&bg_color=1C182D&text_color=C1B1FF&border_color=40317A" className='h-[12rem] w-auto' alt="languages graph" />
           {/* Streak Graph */}
-          <img src="https://streak-stats.demolab.com?user=fwedwicc&locale=en&theme=transparent&bg_color=1C182D&text_color=C1B1FF&hide_border=true&order=3" className='h-[12rem] w-auto bg-[#1C182D] border-[#40317A] border rounded-lg' alt="streak graph" />
+          <img src="https://streak-stats.demolab.com?user=fwedwicc&theme=material-palenight&currStreakNum=C1B1FF&background=1C182D&border=40317A&currStreakLabel=C1B1FF&fire=F1E05A&ring=C693EA&dates=FFFFFF&sideNums=C1B1FF&sideLabels=C1B1FF&stroke=AF84FF36" className='h-[12rem] w-auto' alt="streak graph" />
         </div>
         {/* Contribution Graph */}
         <img src="https://github-readme-activity-graph.vercel.app/graph?username=fwedwicc&radius=7&theme=material-palenight&bg_color=1C182D&text_color=ffffff&area=true&order=5&hide_title=true&hide_border=true" className='lg:h-[20rem] h-[11rem] w-auto rounded-2xl' alt="activity-graph graph" />
@@ -54,7 +54,7 @@ const Certifications = () => {
           ))}
         </div>
       </div>
-      <a href={certificates.link}>
+      <a href={certificates.link} className='z-20'>
         <Button text={certificates.btnText}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
             <path fillRule="evenodd" d="M8.25 3.75H19.5a.75.75 0 0 1 .75.75v11.25a.75.75 0 0 1-1.5 0V6.31L5.03 20.03a.75.75 0 0 1-1.06-1.06L17.69 5.25H8.25a.75.75 0 0 1 0-1.5Z" clipRule="evenodd" />
